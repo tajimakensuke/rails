32ed40b0d5c7294a97a12df604a13bf2613ebefb
EC2 Default User <ec2-user@ip-172-31-44-127.ec2.internal>
2021-03-10 13:41:38 +0000

    [add2]3-4

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,9 +1,9 @@
 class TodolistsController < ApplicationController
-  
+
   def new
     # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
     @list = List.new
-end
+  end
 
   def create
     # １. データを新規登録するためのインスタンス作成
